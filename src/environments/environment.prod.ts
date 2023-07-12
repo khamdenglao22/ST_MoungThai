0ca290d0fe7697d697abfd82cb85a7b7e2aec8d9
vongthaya phounsavanh <kuphounsavanh@gmail.com>
2023-07-12 21:06:00 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,15 +3,5 @@ export const environment = {
   apiUrl: "",
   public_dev: "",
   phetsarathUrl: "",
-  baseUrl: "",
-  // firebase: {
-  //   apiKey: "AIzaSyCIEVhym24hLFa1di_-4reu2GWezoh2faA",
-  //   authDomain: "sokxayall-a193b.firebaseapp.com",
-  //   projectId: "sokxayall-a193b",
-  //   storageBucket: "sokxayall-a193b.appspot.com",
-  //   messagingSenderId: "312289947525",
-  //   appId: "1:312289947525:web:0a0ad39ad3492c6dd4deec",
-  //   measurementId: "G-TVVQHQ90NL",
-  //   vapidKey: "BBPG6YJNXEnfwsaQd5bXD16_jqDYIfrHxX1lg7DAmcvhRZyXfvAIXVPCmjS9cbLsKXOrAvWmTKLXotw_G0jbcDw"
-  // }
+  baseUrl: ""
 };
