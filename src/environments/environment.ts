b167944af3231d59ca1be72ce677b5bb7cb92faf
khamdenglao22 <128339078+khamdenglao22@users.noreply.github.com>
2024-09-19 10:57:36 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,5 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://19.168.1.22:8000',
-  // apiUrl: "http://141.164.110.54:8000",
-  // apiUrl: 'https://34.92.144.38',
-  // apiUrl: 'https://www.stmuangthai.com',
-  apiUrl: 'http://localhost:8888',
-  baseUrl: '',
-  phetsarathUrl: '',
+  
 };
